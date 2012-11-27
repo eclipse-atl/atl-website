6eaf07a485b0a831da2dfee66064abcf2103e333
wpiers <wpiers>
2012-11-27 15:01:27 +0000

    fixed downloads

--- _projectCommon.php
+++ _projectCommon.php
@@ -6,6 +6,17 @@
 	# Optional: defaults to system theme 
 	$theme = "Nova";
 	
+
+	# Define your project-wide Nav bars here.
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# these are optional
+	$Nav->setLinkList(array());
+	$Nav->addNavSeparator("ATL", 	"/atl");
+	$Nav->addCustomNav("Download", "/atl/download", "_self", 3);
+	$Nav->addCustomNav("Documentation", "/atl/documentation", "_self", 3);
+	$Nav->addCustomNav("Support", "/atl/support", "_self", 3);
+	$Nav->addCustomNav("Getting Involved", "/atl/developers", "_self", 3);
+	
 	$pageKeywords	= "atl, dsl, modeling, domain specific language";
 	$pageAuthor		= "Obeo";
 	$pageTitle 		= "ATL";
