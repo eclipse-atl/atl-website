c0b21a7bea688082a8a011be62702297d4b4e05d
Dennis Wagelaar <dwagelaar@gmail.com>
2018-05-14 16:49:37 +0200

    534384: The General Data Protection Regulation (GDPR) at the Eclipse Foundation
    Code formatting
    
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=534384

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php  																														
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
-$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		
-include($App->getProjectCommon());
-include("script/rss2html.php");
-include("script/rss2html_blog.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	
+
+	include($App->getProjectCommon());
+	include("script/rss2html.php");
+	include("script/rss2html_blog.php");
 	
+	$App 	= new App();
+	$Nav	= new Nav();
+	$Menu 	= new Menu();		
+
 	$html = file_get_contents('_index.html');
 
 	# Generate the web page
