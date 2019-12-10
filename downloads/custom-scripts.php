cb89b7b979955330c847d6d7482206bf51f4d551
Dennis Wagelaar <dwagelaar@gmail.com>
2019-12-10 16:56:52 +0100

    558009: [release] modeling.mmt.atl 4.2.0
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=558009

--- downloads/custom-scripts.php
+++ downloads/custom-scripts.php
@@ -302,7 +302,8 @@
 		// UPDATE SITE
 		$buildHTML .= "<tr class=\"repo-info\">";
 		$buildHTML .= "<td><img src=\"" . $websiteRoot . "/images/22/package-x-generic.png\" alt=\"composite update site\"/></td>";
-		if (is_dir(getPWD("updates/" . $typeUpdateSite . "/" . $branch))) {
+		$PWDupdates = getPWD("updates/" . $typeUpdateSite . "/" . $branch);
+		if (is_dir("$PWDupdates")) {
 			$buildHTML .= "<td><b><a href=\"http://download.eclipse.org/" . $PR . "/updates/" . $typeUpdateSite . "/" . $branch . "/" . $ID . "\">Update Site</a></b> for use with <a href=\"http://help.eclipse.org/indigo/index.jsp?topic=/org.eclipse.platform.doc.user/tasks/tasks-127.htm\">p2</a>.</td>";
 		} else {
 			$buildHTML .= "<td><b><a href=\"http://download.eclipse.org/" . $PR . "/updates/" . $typeUpdateSite . "/" . $version . "/" . $ID . "\">Update Site</a></b> for use with <a href=\"http://help.eclipse.org/indigo/index.jsp?topic=/org.eclipse.platform.doc.user/tasks/tasks-127.htm\">p2</a>.</td>";
